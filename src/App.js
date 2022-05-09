248cbd68cad39e7b4ec205f48f2075f768f61df3
Sajadmhy <sajad.mahyaei@gmail.com>
2022-05-09 04:51:01 -0400

    Set up color changes for conditions and disable second pass input

--- src/App.js
+++ src/App.js
@@ -3,25 +3,36 @@ import './App.css';
 
 function App() {
 
-  let [valueUser,setValueUser] = useState();
-  let [passVal,setPassVal] = useState();
-  let [conPassVal,setConPassVal] = useState();
+  const [valueUser,setValueUser] = useState();
+  const [passVal,setPassVal] = useState();
+  const [conPassVal,setConPassVal] = useState();
+  const [isDis, setIsDis] = useState(false);
 
   function handleChangeUser(e) {
-    setValueUser(valueUser=e.target.value);
+    setValueUser(e.target.value);
   };
 
   function handleChangePass(e) {
-    setPassVal(passVal = e.target.value);
+    setPassVal(e.target.value);
   };
 
   function handleChangeCon(e) {
-    setConPassVal(conPassVal = e.target.value)
+    setConPassVal(e.target.value)
   };
+
+  function passCheck() {
+
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+
 
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Sign Up</h2>
         <label id='email'>
           Phone Number or Email*
@@ -34,13 +45,14 @@ function App() {
         <label id='passComf'>
           Confirm Password*
         </label>
-        <input className='con-pass' name='passComf' type='password' value={conPassVal} onChange={handleChangeCon} required readOnly/>
+        <input className='con-pass disable' id='con-pass' name='passComf'
+         type='password' value={conPassVal} onChange={handleChangeCon} required disabled={!isDis}/>
         <div>
-          Password Condition:
-          <p>Password must contain English characters.</p>
-          <p>Password should be at least 6 characters.</p>
-          <p>Password must use letter and numbers.</p>
-          <p>Password and confirm password should be the same.</p>
+          <b>Password Condition:</b>
+          <p id='condition'>Password must contain English characters.</p>
+          <p id='condition'>Password should be at least 6 characters.</p>
+          <p id='condition'>Password must use letter and numbers.</p>
+          <p id='condition'>Password and confirm password should be the same.</p>
         </div>
         <button type='submit'>Sign Up</button>
       </form>
